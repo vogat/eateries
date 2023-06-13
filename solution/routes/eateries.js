eaa246ed4775c596f5b109437dc1fa5cfccc4ba9
vogat <james.voight@me.com>
2023-06-13 17:12:44 -0400

    pin working

--- solution/routes/eateries.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var eateriesCtrl = require('../controllers/eateries');
-
-router.get('/', eateriesCtrl.index);
-router.get('/:id', eateriesCtrl.show);
-
-module.exports = router;
