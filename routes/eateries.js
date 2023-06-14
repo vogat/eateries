8bde9ee183c6b5ba99cc2c4151e0d6f279759d1b
vogat <james.voight@me.com>
2023-06-14 09:04:13 -0400

    cleaned code

--- routes/eateries.js
+++ routes/eateries.js
@@ -3,22 +3,6 @@ var router = express.Router();
 
 var eateriesCtrl = require('../controllers/eateries');
 
-// router.get('/', function(req, res, next) {
-//   res.render('eateries/list');
-// });
-
-// router.get('/3226', function(req, res, next) {
-//   res.render('eateries/3226');
-// });
-
-// router.get('/5475', function(req, res, next) {
-//   res.render('eateries/5475');
-// });
-
-// router.get('/9567', function(req, res, next) {
-//   res.render('eateries/9567');
-// });
-
 router.get('/', eateriesCtrl.index);
 
 router.get('/:id', eateriesCtrl.show);
